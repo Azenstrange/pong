104456078e92996e349046a6d34ddcd886eeac8e
gabrielseron <gabriel.seron.pro@gmail.com>
2022-09-08 18:25:17 +0200

    fix imports

--- src/app/components/components.module.ts
+++ src/app/components/components.module.ts
@@ -2,17 +2,20 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NavbarComponent } from './navbar/navbar.component';
 import { IonicModule } from '@ionic/angular';
+import { BarComponent } from './bar/bar.component';
 
 @NgModule({
   declarations: [
-    NavbarComponent
+    NavbarComponent,
+    BarComponent
   ],
   imports: [
     CommonModule,
     IonicModule
   ],
   exports: [
-    NavbarComponent
+    NavbarComponent,
+    BarComponent
   ]
 })
 export class ComponentsModule { }
