8746b4bb7427e6946a37da2a236a48f016dd23ca
Killian-Flohic <killian.flohic@gmail.com>
2022-09-08 18:28:33 +0200

    game

--- src/app/pages/game/game.page.ts
+++ src/app/pages/game/game.page.ts
@@ -7,9 +7,41 @@ import { Component, OnInit } from '@angular/core';
 })
 export class GamePage implements OnInit {
 
+  gameStatus:any
+  response = {
+    p1Y: 50,
+    p2Y: 50,
+    ball: {
+      x: 50,
+      y: 50
+    },
+    collision : false
+  }
+  bar1:any
+  bar2:any
+  ball:any
+  gameWindow:any
+  width:number
+  height:number
+
   constructor() { }
 
   ngOnInit() {
+    this.bar1 = document.querySelector("#bar1")
+    this.bar2 = document.querySelector("#bar2")
+    this.ball = document.querySelector("#ball")
+    this.gameWindow = document.querySelector(".pong_main_game")
+    this.width = this.gameWindow.width * devicePixelRatio
+    this.height = this.gameWindow.height * devicePixelRatio
+
+    this.defineValues()
   }
 
+  defineValues() {
+
+  }
+  
 }
+
+
+
