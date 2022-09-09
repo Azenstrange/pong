28d755afa49255a214abd91a7ff29e7c85f20166
Killian-Flohic <killian.flohic@gmail.com>
2022-09-09 15:46:07 +0200

    calculs + positions game

--- src/app/pages/game/game.page.ts
+++ src/app/pages/game/game.page.ts
@@ -9,11 +9,11 @@ export class GamePage implements OnInit {
 
   gameStatus:any
   response = {
-    p1Y: 50,
-    p2Y: 50,
+    p1Y: 100,
+    p2Y: 100,
     ball: {
-      x: 50,
-      y: 50
+      x: 100,
+      y: 100
     },
     collision : false
   }
@@ -23,22 +23,66 @@ export class GamePage implements OnInit {
   gameWindow:any
   width:number
   height:number
+  barHeight:number
+  barWidth:number
+  ballHeight:number
+  minBarTravel:number
+  maxBarTravel:number
+  minBallTravelVertical:number
+  maxBallTravelVertical:number
+  minBallTravelHorizontal:number
+  maxBallTravelHorizontal:number
+
 
   constructor() { }
 
   ngOnInit() {
+
+    setTimeout(() => {
+      this.declareElements()
+
+      this.defineValues()
+      window.addEventListener('resize', () => {
+        this.defineValues()
+      })
+
+      this.calculation()
+      setInterval(() => {
+        this.calculation()
+      }, 16)
+
+    }, 1000)
+  }
+
+  declareElements(){
     this.bar1 = document.querySelector("#bar1")
     this.bar2 = document.querySelector("#bar2")
     this.ball = document.querySelector("#ball")
     this.gameWindow = document.querySelector(".pong_main_game")
-    this.width = this.gameWindow.width * devicePixelRatio
-    this.height = this.gameWindow.height * devicePixelRatio
-
-    this.defineValues()
   }
 
   defineValues() {
+    this.width = this.gameWindow.scrollWidth * devicePixelRatio
+    this.height = this.gameWindow.scrollHeight * devicePixelRatio
+    this.barHeight = this.bar1.scrollHeight * devicePixelRatio
+    this.barWidth = this.bar1.scrollWidth * devicePixelRatio
+    this.ballHeight = this.ball.scrollHeight * devicePixelRatio
 
+    this.minBarTravel = this.barHeight/2
+    this.maxBarTravel = this.height - this.barHeight
+    this.minBallTravelVertical = this.ballHeight/2
+    this.maxBallTravelVertical = this.height - this.ballHeight
+    this.minBallTravelHorizontal = this.barWidth + this.ballHeight/2
+    this.maxBallTravelHorizontal = this.width - this.barWidth*2 - this.ballHeight
+
+    // console.log(this.maxBallTravelHorizontal)
+  }
+
+  calculation(){
+    this.bar1.style.top = this.minBarTravel + this.maxBarTravel/100 * this.response.p1Y + "px"
+    this.bar2.style.top = this.minBarTravel + this.maxBarTravel/100 * this.response.p2Y + "px"
+    this.ball.style.top = this.minBallTravelVertical + this.maxBallTravelVertical/100 * this.response.ball.y + "px"
+    this.ball.style.left = this.minBallTravelHorizontal + this.maxBallTravelHorizontal/100 * this.response.ball.x + "px"
   }
   
 }
