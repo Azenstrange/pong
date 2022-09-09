03f6ab7dbce8c9c8d3fb4101f17859a0c6d809fa
Azenstrange <edouard.eisenfisz@gmail.com>
2022-09-09 12:13:53 +0200

    modal moche

--- src/app/pages/room/room.page.ts
+++ src/app/pages/room/room.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ActionSheetController } from '@ionic/angular';
+
 
 @Component({
   selector: 'app-room',
@@ -6,10 +8,33 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./room.page.scss'],
 })
 export class RoomPage implements OnInit {
+  presentingElement = undefined;
 
-  constructor() { }
+  constructor(private actionSheetCtrl: ActionSheetController) { }
 
   ngOnInit() {
+    this.presentingElement = document.querySelector('.ion-page')
   }
+  canDismiss = async () => {
+    const actionSheet = await this.actionSheetCtrl.create({
+      header: 'Are you sure?',
+      buttons: [
+        {
+          text: 'Yes',
+          role: 'confirm',
+        },
+        {
+          text: 'No',
+          role: 'cancel',
+        },
+      ],
+    });
+
+    actionSheet.present();
+
+    const { role } = await actionSheet.onWillDismiss();
+
+    return role === 'confirm';
+  };
 
 }
